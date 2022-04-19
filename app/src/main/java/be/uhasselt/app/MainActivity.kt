956138dd294a9ff3f9bf08ec3b56328b0d1d04f4
lowie2727 <lowie.vanvyve@student.uhasselt.be>
2022-04-19 21:08:18 +0200

    button with snackbar

--- app/src/main/java/be/uhasselt/app/MainActivity.kt
+++ app/src/main/java/be/uhasselt/app/MainActivity.kt
@@ -2,10 +2,23 @@ package be.uhasselt.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import be.uhasselt.app.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        binding.button.setText("test knop")
+
+        binding.button.setOnClickListener { view ->
+            Snackbar.make(view, "Nice, clicked a button", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
     }
 }
