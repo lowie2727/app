de5d1eed9c4f99683961caca07ae8e5c557ebf91
lowie2727 <lowie.vanvyve@student.uhasselt.be>
2022-05-10 11:00:16 +0200

    added navigation drawer with no functionality

--- app/src/main/java/be/uhasselt/app/MainActivity.kt
+++ app/src/main/java/be/uhasselt/app/MainActivity.kt
@@ -1,23 +1,31 @@
 package be.uhasselt.app
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.view.View
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
-import be.uhasselt.app.databinding.ActivityMainBinding
-import be.uhasselt.app.net.LL2Request
-import com.google.android.material.snackbar.Snackbar
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
 
-        setContentView(binding.root)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        actionBarDrawerToggle =
+            ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            true
+        } else super.onOptionsItemSelected(item)
     }
 }
