572007992c8fcd2a00398a3c8d70da175fdafcde
lowie2727 <lowie.vanvyve@student.uhasselt.be>
2022-05-12 01:18:47 +0200

    implementation account creation with appwrite
    Accounts can be created but if invalid details are entered when registering, the app crashes.

--- app/src/main/java/be/uhasselt/app/fragments/LoginFragment.kt
+++ app/src/main/java/be/uhasselt/app/fragments/LoginFragment.kt
@@ -8,6 +8,11 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import be.uhasselt.app.R
 import be.uhasselt.app.databinding.LoginFragmentBinding
+import com.google.android.material.snackbar.Snackbar
+import io.appwrite.Client
+import io.appwrite.services.Account
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class LoginFragment : Fragment(R.layout.login_fragment) {
 
@@ -27,10 +32,38 @@ class LoginFragment : Fragment(R.layout.login_fragment) {
     }
 
     private fun login(view: View) {
+        val email = binding.editTextRegisterEmailAddress.text.toString()
+        val password = binding.editTextRegisterPassword.text.toString()
+
+        if (email.isEmpty()) {
+            msg("email is required", view)
+            return
+        } else if (password.isEmpty()) {
+            msg("password can't be empty", view)
+            return
+        }
+
+        val client = Client(requireContext())
+            .setEndpoint("https://appwrite.lowie.xyz/v1") // Your API Endpoint
+            .setProject("627bac3c2508bec32bc2") // Your project ID
+
+        val account = Account(client)
+
+        GlobalScope.launch {
+            account.createSession(
+                email = email,
+                password = password
+            )
+        }
         findNavController().navigate(R.id.action_login_fragment_to_first_fragment)
     }
 
     private fun goToCreateAccount(view: View) {
         findNavController().navigate(R.id.action_login_fragment_to_register_fragment)
+    }
+
+    private fun msg(text: String, view: View) {
+        Snackbar.make(view, text, Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show()
     }
 }
--- app/src/main/java/be/uhasselt/app/fragments/RegisterFragment.kt
+++ app/src/main/java/be/uhasselt/app/fragments/RegisterFragment.kt
@@ -8,6 +8,10 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import be.uhasselt.app.R
 import be.uhasselt.app.databinding.RegisterFragmentBinding
+import com.google.android.material.snackbar.Snackbar
+import io.appwrite.Client
+import io.appwrite.services.Account
+import kotlinx.coroutines.*
 
 class RegisterFragment : Fragment(R.layout.register_fragment) {
 
@@ -20,12 +24,50 @@ class RegisterFragment : Fragment(R.layout.register_fragment) {
     ): View {
         binding = RegisterFragmentBinding.inflate(layoutInflater)
 
-        binding.buttonCreateAccount.setOnClickListener(this::register)
+        binding.registerButtonCreateAccount.setOnClickListener(this::createAccount)
 
         return binding.root
     }
 
-    private fun register(view: View) {
-        findNavController().navigate(R.id.action_register_fragment_to_first_fragment)
+    private fun createAccount(view: View) {
+        val userId = binding.editTextRegisterUserName.text.toString()
+        val email = binding.editTextRegisterEmailAddress.text.toString()
+        val password = binding.editTextRegisterPassword.text.toString()
+        val name = binding.editTextRegisterFullName.text.toString()
+
+        if (name.isEmpty()) {
+            msg("please fill in your full name", view)
+            return
+        } else if (userId.isEmpty()) {
+            msg("name is required", view)
+            return
+        } else if (email.isEmpty()) {
+            msg("email is required", view)
+            return
+        } else if (password.isEmpty()) {
+            msg("password can't be empty", view)
+            return
+        }
+
+        val client = Client(requireContext())
+            .setEndpoint("https://appwrite.lowie.xyz/v1") // Your API Endpoint
+            .setProject("627bac3c2508bec32bc2") // Your project ID
+            .setSelfSigned(true)
+
+        val account = Account(client)
+
+        GlobalScope.launch {
+            account.create(
+                userId = userId,
+                email = email,
+                password = password,
+            )
+        }
+        findNavController().navigate(R.id.action_register_fragment_to_login_fragment)
+    }
+
+    private fun msg(text: String, view: View) {
+        Snackbar.make(view, text, Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show()
     }
 }
